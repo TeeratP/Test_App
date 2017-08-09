c5f9f5db85961506a40956e95914f08c640fa044
TeeratP <teerat.pipitcharulerd@gmail.com>
2017-08-09 17:22:23 +0800

    Change app to emoji stupid app, watch upto sending something to the next page

--- Second App/ViewController.swift
+++ Second App/ViewController.swift
@@ -8,31 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
     
-    var counter = 0
+    @IBOutlet weak var TableView_1: UITableView!
     
-    @IBOutlet weak var Label_1: UILabel!
-    @IBAction func Button_1(_ sender: UIButton) {
-        
-        Label_1.text = "Answer is: \(Double(Text_1.text!)! + Double(Text_2.text!)!)"
-    }
-    
-    @IBOutlet weak var Text_1: UITextField!
-    @IBOutlet weak var Text_2: UITextField!
+    var emojis = ["😀","😡","😩","😍","😇","😵"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        TableView_1.dataSource = self
+        TableView_1.delegate = self
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+    }
     
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let emoji = emojis[indexPath.row]
+        performSegue(withIdentifier: "moveSegue", sender: emoji)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        print(sender)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
